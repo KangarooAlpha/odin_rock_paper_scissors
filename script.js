0c5c30e08b53cd9d32fb97787ffa17eace8069c9
Omar <omar.fayyaz@ryerson.ca>
2024-07-14 12:05:34 -0400

    finished playRound function

--- script.js
+++ script.js
@@ -10,22 +10,40 @@ function getComputerChoice(){
         return ("scissors")
     }
 }
-console.log(getComputerChoice())
+
 
 function getHumanChoice(){
     let choice = prompt('Please choose between rock, paper and scissors.').toLowerCase()
     if (!(choice == 'rock' || choice == 'paper' || choice == 'scissors')){
-        return choice = getHumanChoice()
+        return (choice = getHumanChoice())
     }
-    else{
-        return choice;
-    }
+    return choice 
 }
-
 
 let humanscore = 0;
 let computerscore = 0;
 
 function playRound (humanChoice, computerChoice){
-
+    if (humanChoice == computerChoice) {
+        return ("draw")
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper" || 
+        humanChoice == "paper" && computerChoice == "scissors" ||
+        humanChoice == "scissors" && computerChoice == "rock") {
+        return ("lose")
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissors" ||
+        humanChoice == "paper" && computerChoice == "rock" ||
+        humanChoice == "scissors" && computerChoice == "paper") {
+            return ("win")
+        }
 }
+
+let hChoice = getHumanChoice()
+let cChoice = getComputerChoice()
+
+console.log(hChoice)
+console.log(cChoice)
+
+console.log(playRound(hChoice,cChoice))
+

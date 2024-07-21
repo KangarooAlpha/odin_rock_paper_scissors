e8f32a37ca6eb6c8b1c4394f30d4549206ef95f6
Omar <omar.fayyaz@ryerson.ca>
2024-07-20 20:33:10 -0400

    user input modifications

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*3)
     if (choice == 0){
+        console.log("rock")
         return ("rock")
     }
     else if (choice == 1){
+        console.log(paper)
         return ("paper")
     }
     else {
@@ -11,15 +13,21 @@ function getComputerChoice(){
     }
 }
 
+const buttons = document.querySelectorAll("button") 
 
 function getHumanChoice(){
-    let choice = prompt('Please choose between rock, paper and scissors.').toLowerCase()
-    if (!(choice == 'rock' || choice == 'paper' || choice == 'scissors')){
-        return (choice = getHumanChoice())
-    }
-    return choice 
-}
-
+    rockBtn.addEventListener("click", ()=> {
+        console.log(rockBtn.id)
+        return (rockBtn.id)
+    });
+    paperBtn.addEventListener("click" , () => {
+        return (paperBtn.id)
+    });
+    scissorsBtn.addEventListener("click" , ()=> {
+        return (scissorsBtn.id)
+    });
+    }; 
+     
 let humanscore = 0;
 let computerscore = 0;
 
@@ -40,22 +48,27 @@ function playRound (humanChoice, computerChoice){
 }
 
 function playMatch(){
-    while(humanscore < 5 && computerscore < 5) {
-        let hChoice = getHumanChoice();
-        let cChoice = getComputerChoice();
-        let round = playRound(hChoice,cChoice)
+    //while(humanscore < 5 && computerscore < 5) {
+    for (let i = 0; i < 5; i++){
+        //let hChoice = getHumanChoice()
+        //let cChoice = getComputerChoice()
+        let round = playRound(getHumanChoice(),getComputerChoice())
         if (round == 'win'){
             humanscore += 1;
+            console.log('rw')
             addToList("You win this round " + hChoice + " beats " + cChoice + ".");
         }
         else if (round == 'lose') {
             computerscore += 1;
+            console.log('rl')
             addToList("You lose this round " + cChoice + " beats " + hChoice + ".");
         }
         else if (round == 'draw') {
+            console.log('da')
             addToList("This round was a draw!")
         }
     }
+    if (humanscore >= 5 || computerscore >= 5){
     if (humanscore > computerscore) {
         addToList("Congrats you won this match! Your score was " + humanscore + " while " +
             "the computer scored " + computerscore)
@@ -68,21 +81,23 @@ function playMatch(){
         addToList("This round was tie! You and the computer both score " + humanscore)
     }
 }
+}
 
 const container = document.querySelector(".container");
 const rockBtn = document.querySelector("#rock");
 const paperBtn = document.querySelector("#paper");
 const scissorsBtn = document.querySelector("#scissors");
+const ul = document.createAttribute("ul");
 
 function addToList(string){
     var ul = document.createAttribute("ul");
     var span = document.createAttribute("span");
 
     span.textContent = (string)
+    console.log(string)
     
-    ul.appendChild(span);
-    container.appendChild(ul);
+    ul.appendChild(span)
+    container.appendChild(ul)
 }
 
-
-
+playMatch()
